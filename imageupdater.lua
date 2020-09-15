0a98eced72802d155e9c7a4db8c26e8951dc838a
autozfarmz <71283813+autozfarmz@users.noreply.github.com>
2020-09-14 17:25:26 -0700

    Update imageupdater.lua

--- imageupdater.lua
+++ imageupdater.lua
@@ -1,5 +1,5 @@
 localPath = scriptPath()
-imagePath = (localPath .. "images/")
+imagePath = (localPath .. "luar/images/")
 httpDownload("https://raw.githubusercontent.com/autozfarmz/auto1200x1920/master/luar/1farmImage/packx.png", imagePath .."packx.png")
 
 scriptExit("You have Updated the auto zFarmz image library!")
